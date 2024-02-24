d1ccadc9b26a1f469d0658e26f3be306d971e19f
Benjamin Antupit <b@antupit.com>
2024-02-24 14:47:32 -0500

    adapt to new standard node

--- ros-nucleo_test/src/main.cpp
+++ ros-nucleo_test/src/main.cpp
@@ -9,37 +9,59 @@
 
 // ----- MAIN CONVEYOR -----
 
-int BEAM_BREAK_PIN = D2; 
+MODULE* main_conveyor_module;
+int EDGE_BEAM_BREAK_PIN = D2; 
+int CENTER_BEAM_BREAK_PIN = D3; //TODO: VERIFY PIN NUMBER
 int SPEED_PIN = A0;
 int INVERT_PIN = D13;
 
-// -- conveyor feedback 
-std_msgs::Int8 conveyor_msg;
-String _conveyor_topic(NODE_NAME + "_feedback__conveyor");
-ros::Publisher conveyor_feedback_pub(_conveyor_topic.c_str(), &conveyor_msg);
-
 enum CONVEYOR_STATE {
     CONVEYOR_IDLE = 0, 
-    MOVING_TO_CENTER = 1, 
-    MOVING_TO_NEXT_DISC = 2, 
-    BACKUP = 3};
-
+    ADVANCING_TO_NEXT_DISC_EDGE = 1,
+    WAITING_FOR_INTAKE = 2,
+    MOVING_TO_CENTER = 3, 
+    BACKUP = 4};
 CONVEYOR_STATE conveyor_state = CONVEYOR_STATE::CONVEYOR_IDLE; 
+
 long last_conveyor_center_time = millis();
 
 void move_forward(int speed = 230) {
   digitalWrite(INVERT_PIN, LOW);
   analogWrite(SPEED_PIN, speed); // start
-  loginfo("move forward");
+  loginfo("conveyor moving forward");
 }
 
 void move_backward(int speed = 230) {
   digitalWrite(INVERT_PIN, HIGH);
   analogWrite(SPEED_PIN, speed); // start
-  loginfo("move backward");
+  loginfo("conveyor moving backward");
 }
 
-void stop() {
+bool edge_beam_broken() {
+  return (digitalRead(EDGE_BEAM_BREAK_PIN) == 0);
+}
+
+bool center_beam_broken() {
+  return (digitalRead(CENTER_BEAM_BREAK_PIN) == 0);
+}
+
+void start_conveyor() {
+  if (conveyor_state == CONVEYOR_STATE::CONVEYOR_IDLE) {
+    loginfo("start_conveyor in IDLE --> moving to center");
+    conveyor_state = CONVEYOR_STATE::ADVANCING_TO_NEXT_DISC_EDGE;
+    move_forward();
+  } else if (conveyor_state == CONVEYOR_STATE::WAITING_FOR_INTAKE) {
+    loginfo("start_conveyor in WAITING FOR INTAKE --> advancing to next disc edge");
+    conveyor_state = CONVEYOR_STATE::MOVING_TO_CENTER;
+    move_forward();
+  } else {
+    logwarn("start_conveyor called in invalid state, " + String((int)conveyor_state));
+  
+  }
+  
+}
+
+void stop_conveyor() {
   analogWrite(SPEED_PIN, 0); // stop
   if (conveyor_state != CONVEYOR_STATE::CONVEYOR_IDLE) {
     loginfo("stop");
@@ -47,61 +69,51 @@ void stop() {
   }
 }
 
-bool beam_broken() {
-  return (digitalRead(BEAM_BREAK_PIN) == 0);
-}
-
-void start_conveyor() {
-  loginfo("start_conveyor");
-  conveyor_state = CONVEYOR_STATE::MOVING_TO_CENTER;
-  move_forward();
+void calibrate_conveyor() {
+  loginfo("calibrate conveyor; TODO"); //TODO: Implement calibration
 }
 
 void check_conveyor() {
   switch (conveyor_state){
-    case CONVEYOR_STATE::MOVING_TO_CENTER:
-      if (beam_broken()) {
-        conveyor_state = CONVEYOR_STATE::MOVING_TO_NEXT_DISC;
-        last_conveyor_center_time = millis();
-      } 
+    case CONVEYOR_STATE::ADVANCING_TO_NEXT_DISC_EDGE:
+      if (edge_beam_broken()) {
+        stop_conveyor();
+        conveyor_state = CONVEYOR_STATE::WAITING_FOR_INTAKE;
+      }
       break;
-    case CONVEYOR_STATE::MOVING_TO_NEXT_DISC:
-      if (beam_broken() && last_conveyor_center_time+1000 < millis()) {
-        move_backward();
+    case CONVEYOR_STATE::WAITING_FOR_INTAKE:
+      // next state is triggered by signal from intake module via Pi
+      break;
+    case CONVEYOR_STATE::MOVING_TO_CENTER:
+      if (center_beam_broken()) {
         conveyor_state = CONVEYOR_STATE::BACKUP;
-        last_conveyor_center_time = millis();
-      } 
+        move_backward();
+      }
       break;
     case CONVEYOR_STATE::BACKUP:
-      if (last_conveyor_center_time+500 < millis()) {
-        stop();
+      if (edge_beam_broken()) {
+        stop_conveyor();
         conveyor_state = CONVEYOR_STATE::CONVEYOR_IDLE;
-        send_status(NODE_STATUS::MOTION_COMPLETE);
-        status.data = NODE_STATUS::IDLE;
+        main_conveyor_module->publish_status(MODULE_STATUS::COMPLETE);
       }
       break;
     case CONVEYOR_STATE::CONVEYOR_IDLE:
-      stop();
+      stop_conveyor();
       break;
     default:
       logwarn("Invalid conveyor state");
       break;
   }
-
-  // publish state to pi 
-  if (conveyor_msg.data != (int) conveyor_state) {
-      conveyor_msg.data = conveyor_state;
-      loginfo("publishing conveyor state");
-      conveyor_feedback_pub.publish(&conveyor_msg);
-  }
+  main_conveyor_module->publish_state((int) conveyor_state);
 }
 
-bool verify_motion_complete() {
+bool verify_conveyor_complete() {
   return conveyor_state == CONVEYOR_STATE::CONVEYOR_IDLE;
 }
 
 // ----- SCALE -----
 
+MODULE* scale_module;
 #define SCALE_SERIAL__RX_PIN D4
 #define SCALE_SERIAL__TX_PIN D5 //not used
 
@@ -114,7 +126,7 @@ float lastWeight = 0.0;
 char receivedChars[numChars];
 
 std_msgs::Float32 weight_msg;
-String _weight_topic(NODE_NAME + "_feedback__weight");
+String _weight_topic("scale_feedback__weight");
 ros::Publisher weight_feedback_pub(_weight_topic.c_str(), &weight_msg);
 
 enum SCALE_STATE {
@@ -124,6 +136,7 @@ enum SCALE_STATE {
     POWERING_ON = 3,
     POWERING_OFF = 4};
 SCALE_STATE scale_state = SCALE_STATE::SCALE_IDLE;
+
 unsigned long last_scale_data_time = millis();
 unsigned long start_scale_action_time = millis();
 
@@ -140,19 +153,29 @@ void toggleScaleTare() {
     digitalWrite(SCALE_RELAY__TARE_PIN, LOW);
 }
 
-void start_measure() {
-  loginfo("start_measure");
+void start_scale() {
+  loginfo("start scale");
   start_scale_action_time = millis();
   scale_state = SCALE_STATE::MEASURING;
 }
 
-void check_measure() {
+void stop_scale() {
+  loginfo("stop scale");
+  scale_state = SCALE_STATE::SCALE_IDLE;
+}
+
+void calibrate_scale() {
+  loginfo("calibrate scale; TODO"); //TODO: Implement calibration
+}
+
+void check_scale() {
   switch (scale_state) {
   case SCALE_STATE::MEASURING:
     if (start_scale_action_time+1500 < millis()) { //measurement complete
       weight_msg.data = lastWeight;
-      send_status(NODE_STATUS::MEASURE_COMPLETE);
+      scale_module->publish_status(MODULE_STATUS::COMPLETE);
       scale_state = SCALE_STATE::SCALE_IDLE;
+      loginfo("scale measurement complete");
     }
     break;
   case SCALE_STATE::TARING:
@@ -178,13 +201,15 @@ void check_measure() {
   default:
     break;
   }
+  scale_module->publish_state((int) scale_state);
 
-  if (last_scale_data_time+1000 < millis()) { //If we haven't heard from the scale, turn it on!
-    scale_state = SCALE_STATE::POWERING_ON;
-  }
+  //TODO: Implement scale power on
+  // if (last_scale_data_time+1000 < millis()) { //If we haven't heard from the scale, turn it on!
+  //   scale_state = SCALE_STATE::POWERING_ON;
+  // }
 }
 
-bool verify_measure_complete() {
+bool verify_scale_complete() {
   return scale_state == SCALE_STATE::SCALE_IDLE;
 }
 
@@ -208,10 +233,10 @@ void parseIncomingData() {
                 ndx = 0;
                 if (atoff(receivedChars) != lastWeight) {
                     lastWeight = atoff(receivedChars);
-                    if (scale_state == SCALE_STATE::MEASURING) {
+                    //if (scale_state == SCALE_STATE::MEASURING) {
                       weight_msg.data = lastWeight;
                       weight_feedback_pub.publish(&weight_msg);
-                    }
+                    //}
                 }
             }
         } else if (rc == startMarker) {
@@ -223,19 +248,23 @@ void parseIncomingData() {
 // ----- loop/setup functions -----
 void setup() {
   init_std_node();
-  set_request_callbacks(
-    start_measure, 
-    verify_measure_complete, 
+  scale_module = init_module("scale",
+    start_scale, 
+    verify_scale_complete, 
+    stop_scale,
+    calibrate_scale);
+  main_conveyor_module = init_module("main_conveyor",
     start_conveyor, 
-    verify_motion_complete, 
-    stop);
+    verify_conveyor_complete, 
+    stop_conveyor,
+    calibrate_conveyor);
   
   //Register ROS publishers
   nh.advertise(weight_feedback_pub);
-  nh.advertise(conveyor_feedback_pub);
   
   // conveyor pins 
-  pinMode(BEAM_BREAK_PIN, INPUT_PULLUP) ;
+  pinMode(EDGE_BEAM_BREAK_PIN, INPUT_PULLUP) ;
+  pinMode(CENTER_BEAM_BREAK_PIN, INPUT_PULLUP) ;
   pinMode(SPEED_PIN,OUTPUT) ;
   pinMode(INVERT_PIN, OUTPUT) ;
 
@@ -252,7 +281,7 @@ void loop() {
   periodic_status();
   nh.spinOnce();
   parseIncomingData();
-  check_measure();
+  check_scale();
   check_conveyor();
 
   // ----- testing ----- 
