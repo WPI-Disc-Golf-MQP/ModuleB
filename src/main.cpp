e694acfe00c7b38b1dbd4569a0663a974f0c745f
Anthony Gonzales <amgonzales@wpi.edu>
2025-02-24 16:32:54 -0500

    Changed to run on Samd, pins updated

--- src/main.cpp
+++ src/main.cpp
@@ -272,11 +272,11 @@ bool verify_flex_complete() {
 }
 
 bool run_yaxis_motor = false;
-long yaxis_motor_last_step = millis();
+unsigned long yaxis_motor_last_step = millis();
 bool yaxis_motor_last_digital_write = false;
 
 bool run_spin_motor = false; 
-long spin_motor_last_step = millis();
+unsigned long spin_motor_last_step = millis();
 bool spin_motor_last_digital_write = false;
 
 
